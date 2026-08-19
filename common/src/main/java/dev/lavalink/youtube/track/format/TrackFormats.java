8f0bd940327603581ccc7af0c8c100384fe70af5
KingsMusic <kaozshadowyt@gmail.com>
2026-08-18 22:23:29 -0700

    Update TrackFormats.java

--- common/src/main/java/dev/lavalink/youtube/track/format/TrackFormats.java
+++ common/src/main/java/dev/lavalink/youtube/track/format/TrackFormats.java
@@ -79,6 +79,11 @@ public class TrackFormats {
                 continue;
             }
 
+            if (format.isAutoDubbed()) {
+                // skip auto dubbed formats
+                continue;
+            }
+
             if (isBetterFormat(format, bestFormat)) {
                 bestFormat = format;
             }
